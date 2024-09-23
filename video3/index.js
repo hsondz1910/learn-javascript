9ed108af3b2b48713dcd9df174cea945035ff6ad
hsondz1910 <hsondz1910@gmail.com>
2024-09-23 08:05:47 +0700

    w3schools.com, developer.mozilla.org

---
+++ video3/index.js
@@ -0,0 +1,11 @@
+let a = 5
+let b = 1.5
+let c = 'Pham Hoang Son'
+let d = 156 +'asindians'
+const testString = 'pham hoang son'
+let testNum = typeof d
+// alert('Chào mừng đến với website')
+console.log('Hi ae' + ' ' + 'den voi' + a + b + c + ' ' + d)
+console.log(typeof a+ ' ' + testString);
+console.log(testNum)
+console.log('Test num:', 'a =', 'b =', b, c, d)
