2dc17ea45772f8c948199b1be248d1645c32bef6
hsondz1910 <hsondz1910@gmail.com>
2024-09-23 08:49:54 +0700

    Backtick tips

---
+++ video8/index.js
@@ -0,0 +1,10 @@
+let testString = 'Chao moi nguoi nha hahaha'
+let testString1 = 'Cam on nha'
+let plusString = testString + ' ' + testString1
+let a = 5
+let b = 5
+let c = 5
+let d = 5
+
+console.log(testString, testString1, plusString)
+console.log(a + b + testString1 + c + testString)
